cce5deec1b86244cbd1d1f10e518a1c0c22e1b70
NaxNax666 <NaxNax@MSI-ANDREW>
2022-03-29 23:51:44 +0300

    вроде готово

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -25,8 +25,9 @@ int main(int argc, char** argv)
     if (rank == 0) // Ecam npoyecc HyneBon
     {
         for (i = 0; i < N1; i++) {
-            for (j = 0; j < N2; j++) A[i][j] = i + j; // Manunannaayna maccnea A
-            B[i] = i;                                             // Wanqnannzayna maccnea B
+            A[i] = i * 3;
+            B[i] = i * i;
+            C[i] = 3 * i * i + 1;
         }
 
 
@@ -40,17 +41,15 @@ int main(int argc, char** argv)
 
             for (i = 0; i < N1 / size; i++) // ka poancnennit
             {
-                C[i] = 0;
-                for (j = 0; j < N2; j++)
-                    C[i] += B[i] * A[i][j];
+                Y[i] = (S1 + S2) * B[i] + A[i] / C[i];
             }
-            for (i = 1; i < size; i++) // C6op peaynbtatos 8 maccns C
-                MPI_Recv(&C[(int)i * N1 / size], (int)N1 / size, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
+            for (i = 1; i < size; i++) // C6op peaynbtatos 8 maccns Y
+                MPI_Recv(&Y[(int)i * N1 / size], (int)N1 / size, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
         }
         // Koxey ynkna KpatHocth
         time2 = MPI_Wtime(); // OnpeneneHne epemeHn KoHUa o6pa6orKn
         time = time2 - time1;
-        for (i = 0; i < N1; i++) printf("%f", C[i]); // BuBon pesyrbtaToe
+        for (i = 0; i < N1; i++) printf("%f", Y[i]); // BuBon pesyrbtaToe
 
         if (rank == 0) printf("\nTIME = %f\n", time); // Boisoa spemenh
         // ebinonHeHnA
@@ -59,26 +58,27 @@ int main(int argc, char** argv)
     else //Ecan npowecc He KyjeRon
     {
         for (i = 0; i < N1; i++)
-            for (j = 0; j < N2; j++) A[i][j] = i + j;  // Wnnynannaayna maccnea A
-            // 0 scex npoyecax
+            for (j = 0; j < N2; j++) {
+                A[i] = i * 3;
+                 // Wnnynannaayna maccnea A
+            }  // 0 scex npoyecax
 
 
 
         for (cicle = 0; cicle < 10000; cicle++) // «l|akn KpatHocTA»    
         {
             MPI_Recv(&B[(int)rank * N1 / size], (int)N1 / size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status); // Mpvem anemeHtoa maccnaa B
+            MPI_Recv(&C[(int)rank * N1 / size], (int)N1 / size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
                    // ot Hynesoro npoyecca
 
             for (i = rank * N1 / size; i < (rank + 1) * N1 / size; i++)
             {
-                C[i] = 0;
-                for (j = 0; j < N2; j++)
-                    C[i] += B[i] * A[i][j];
+                Y[i] = (S1 + S2) * B[i] + A[i] / C[i];
 
             }
 
             // Nepegaya pesyrbtatos HyneBomy npoyeccy
-            MPI_Send(&C[(int)rank * N1 / size], (int)N1 / size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&Y[(int)rank * N1 / size], (int)N1 / size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
         }
 
     }
